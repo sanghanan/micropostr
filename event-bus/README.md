7fe71139bb63ee751a3e28aea785814bfc83841e
Sam Singh Anantha <samsinghanantha@gmail.com>
2024-03-06 21:54:40 -0500

    create an event bus

---
+++ event-bus/README.md
@@ -0,0 +1,9 @@
+# Event Bus
+
+The event bus is a simple implementation that allows communication between different components or services in a microservices architecture.
+
+## Usage
+
+1. Install the event bus package:
+   ```bash
+   npm install event-bus
--- posts-server/src/index.ts
+++ posts-server/src/index.ts
@@ -1,6 +1,7 @@
 import express, { Express, Request, Response } from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import axios from "axios";
 
 const app: Express = express();
 app.use(bodyParser.json());
@@ -11,6 +12,7 @@ type Post = {
   id: string;
   content: string;
 };
+const EVENT_BUS_URL = 'http://localhost:4005/events';
 
 const posts: { [id: string]: Post } = {};
 
@@ -22,11 +24,17 @@ app.get('/posts', (req: Request, res: Response) => {
   res.status(200).send(posts);
 });
 
-app.post('/posts', (req: Request, res: Response) => {
+app.post('/posts', async(req: Request, res: Response) => {
   const id = randomBytes(4).toString('hex');
   const content = req.body.content;
   posts[id] = { id, content: content };
+  await axios.post(EVENT_BUS_URL, {type: 'PostCreated', data: {id, content}});
   res.status(201).send(posts[id]);
+});
+
+app.post('/events', (req: Request, res: Response) => {
+  console.log('Received Event', req.body.type);
+  res.send({});
 });
 
 app.listen(PORT, () => {
